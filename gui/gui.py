5ce225d053a07fd7e0428983da81c4228d758bb1
uriar003 <115667293+uriar003@users.noreply.github.com>
2022-11-15 20:55:10 -0800

    Update gui.py
    Update the python file to include kivymd functionality (the list that John created)

--- gui/gui.py
+++ gui/gui.py
@@ -1,12 +1,18 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivymd.uix.list import *
+#from kivy.clock import Clock
+from kivy.uix.image import Image
 
-screen_manager = ScreenManager()
+#screen_manager = ScreenManager()
+
 
 Builder.load_file('frontPage.kv')
 Builder.load_file('login.kv')
-Builder.load_file('mainpos.kv')
+Builder.load_file('POSMDList.kv')
 Builder.load_file('cart.kv')
 Builder.load_file('reports.kv')
 Builder.load_file('addInv.kv')
@@ -35,19 +41,33 @@ class addInv(Screen):
 class account(Screen):
     pass
 
-screen_manager.add_widget(frontPage(name="front"))
-screen_manager.add_widget(login(name="login"))
-screen_manager.add_widget(mainPOS(name="main"))
-screen_manager.add_widget(cart(name="cart"))
-screen_manager.add_widget(reports(name="reports"))
-screen_manager.add_widget(addInv(name="invent"))
-screen_manager.add_widget(account(name="account"))
+#screen_manager.add_widget(frontPage(name="front"))
+#screen_manager.add_widget(login(name="login"))
+#screen_manager.add_widget(mainPOS(name="main"))
+#screen_manager.add_widget(cart(name="cart"))
+#screen_manager.add_widget(reports(name="reports"))
+#screen_manager.add_widget(addInv(name="invent"))
+#screen_manager.add_widget(account(name="account"))
 
-def press(self):
-    pass
+class mainPOS(Screen):
+    def onpress(self, pressed, list_id):
+        item = TwoLineAvatarListItem(text=f"soup", secondary_text=f"$2.99")
+        item.add_widget(IconLeftWidget(icon="soup.png"))
+        self.ids.itemlist.add_widget(item)
 
-class posApp(App):
+
+class posApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(frontPage(name="front"))
+        screen_manager.add_widget(login(name="login"))
+        screen_manager.add_widget(mainPOS(name="main"))
+        screen_manager.add_widget(cart(name="cart"))
+        screen_manager.add_widget(reports(name="reports"))
+        screen_manager.add_widget(addInv(name="invent"))
+        screen_manager.add_widget(account(name="account"))
+
         return screen_manager
 
 if __name__ == '__main__':
