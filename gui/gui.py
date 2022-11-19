eb5c7fd4b35bb363f7eda06e78a7acee7dd9e5b5
John Barbonio <62598528+jybarbonio@users.noreply.github.com>
2022-11-19 10:17:15 -0800

    Add files via upload
    - added skeleton function for 'Generate Reports' button in reports.kv/reports function in gui
    - Added list add and removal functionality in mainPOS
    - 'clear cart' button clears all items/widgets in mdlITEMLIST MDList, emptying cart
    - moved as much of the scanner functionality out of the main build func as I possibly could
    - laid out basic sql query get list format (Ctrl+F lstItem = [ "NAME", "$X.XX", "ABC-abc-1234", 2, "genericitem.png" ] )

--- gui/gui.py
+++ gui/gui.py
@@ -1,11 +1,18 @@
-from kivymd.app import MDApp
+from functools import partial
+
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
-from kivymd.uix.list import *
-#from kivy.clock import Clock
 from kivy.uix.image import Image
+
+from kivymd.app import MDApp
+from kivymd.uix.list import *
+
+import cv2                          # OpenCV is under Apache License 2.0, so it is free to use commercially
+import numpy as np
+from pyzbar.pyzbar import decode    # PyzBar is under the MIT License, which among other things permits modification and re-sale
 #import mysql.connector
 
 #screen_manager = ScreenManager()
@@ -44,19 +51,10 @@ class frontPage(Screen):
 class login(Screen):
     pass
 
-class mainPOS(Screen):
-    pass
-
 class cart(Screen):
     pass
 
-class reports(Screen):
-    pass
-
 class addInv(Screen):
-    pass
-
-class account(Screen):
     pass
 
 #screen_manager.add_widget(frontPage(name="front"))
@@ -68,13 +66,61 @@ class account(Screen):
 #screen_manager.add_widget(account(name="account"))
 
 class mainPOS(Screen):
-    def onpress(self, pressed, list_id):
-        item = TwoLineAvatarListItem(text=f"soup", secondary_text=f"$2.99")
-        item.add_widget(IconLeftWidget(icon="soup.png"))
-        self.ids.itemlist.add_widget(item)
+    # params for additem are as follows
+    # scr is screen for widget (list item to be added)
+    # frame is the frame to be scanned for barcodes by decode func
+    def addscanitem(self, scr, success, frame):
+        # note that decode gets ALL barcodes in a frame, but we only want the first one detected
+        barcodes = decode(frame)
+        bdata = barcodes[0].data.decode('utf-8')
+        print("barcode found: adding ", bdata, " to mem loc ", scr.ids.mdlITEMLIST)
+
+        # placeholder for barcode returning from SQL a list of relevant item properties
+        lstItem = [ "NAME", "$X.XX", "ABC-abc-1234", 2, "genericitem.png" ]
+        
+        # create an item to be added to MDList with evident properties
+        item = ThreeLineAvatarListItem(
+            text = lstItem[0], 
+            secondary_text = lstItem[1], 
+            tertiary_text = lstItem[2]
+            )
+        # add to the MDList item a widget for the icon lest it just be text lines
+        # notice 'item' is itself a widget, so widget within a widget
+        item.add_widget(IconLeftWidgetWithoutTouch(icon = str(lstItem[4])))
+
+        # attach to the MDList item a self-delete upon click function
+        item.bind(on_press = mainPOS().deleteitem)
+        scr.ids.mdlITEMLIST.add_widget(item)
+
+    # placeholder func as of now, later this will cause a popup where one can add an item by barcode lookup
+    def addmanualitem(self, barcode):
+        # some code here to query SQL db, giving it barcode and 
+        # returning name, price, barcode, amount to purchase, icon
+
+        # hypothetical returned data example from SQL query
+        lstItem = [ "NAME", "$X.XX", "ABC-abc-1234", 2, "genericitem.png" ]
+
+        self.item = ThreeLineAvatarListItem(
+            text = lstItem[0], 
+            secondary_text = lstItem[1],
+            tertiary_text = lstItem[2]
+            )
+        self.item.add_widget(IconLeftWidgetWithoutTouch(icon = str(lstItem[4])))
+        self.ids.mdlITEMLIST.add_widget(self.item)
+    
+    # function to self delete MDList item upon click
+    # the button passes itself (obj) as arg and you want to delete it,
+    #   but the func needs to by invoked via the parent, with parameter being the child (obj) list item
+    def deleteitem(self, obj):
+        print("deleting item in: ", self, " | ", obj.parent)
+        obj.parent.remove_widget(obj)
+
+    def deleteallitems(self, obj):
+        print("deleting all items in", self, " | ", obj)
+        self.ids.mdlITEMLIST.clear_widgets()
 
 class reports(Screen):
-    def onpress(self, pressed, list_id):
+    def generatereport(self, *args):
         item = TwoLineAvatarListItem(text=f"Sales Report", secondary_text=f"Week_1")
        #item.add_widget(IconLeftWidget(icon="soup.png"))
         self.ids.itemlist.add_widget(item)
@@ -106,7 +152,7 @@ class helpScreen(Screen):
 class posApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        screen_manager = ScreenManager()
+        self.screen_manager = ScreenManager()
 
         #define data base stuff
         #mydb = mysql.connector.connect(
@@ -126,17 +172,35 @@ class posApp(MDApp):
         #for db in c:
             #print(db)
 
-        screen_manager.add_widget(frontPage(name="front"))
-        screen_manager.add_widget(login(name="login"))
-        screen_manager.add_widget(mainPOS(name="main"))
-        screen_manager.add_widget(cart(name="cart"))
-        screen_manager.add_widget(reports(name="reports"))
-        screen_manager.add_widget(addInv(name="invent"))
-        screen_manager.add_widget(account(name="account"))
-        screen_manager.add_widget(searchItem(name="search"))
-        screen_manager.add_widget(helpScreen(name="help"))
+        self.screen_manager.add_widget(frontPage(name="front"))
+        self.screen_manager.add_widget(login(name="login"))
+        self.screen_manager.add_widget(mainPOS(name="main"))
+        self.screen_manager.add_widget(cart(name="cart"))
+        self.screen_manager.add_widget(reports(name="reports"))
+        self.screen_manager.add_widget(addInv(name="invent"))
+        self.screen_manager.add_widget(account(name="account"))
+        self.screen_manager.add_widget(searchItem(name="search"))
+        self.screen_manager.add_widget(helpScreen(name="help"))
 
-        return screen_manager
+        # check camera every second or so for a barcode
+        # Clock does not like passing a func with params, so oncvscan is a middle man
+        Clock.schedule_interval(self.oncvscan, 1.0/1.0)
+        self.cam = cv2.VideoCapture(1)
+
+        return self.screen_manager
+
+    def oncvscan(self, *args):
+        scr = self.screen_manager.get_screen("main")
+        success, frame = self.cam.read()
+
+        # you don't want camera to add items when off main item screen
+        if(self.screen_manager.current == "main"):
+            # if barcode was detected
+            if(len(decode(frame)) > 0):
+                print("barcode found")
+                mainPOS().addscanitem(scr, success, frame)
+            else:
+                print("barcode not found")
 
 if __name__ == '__main__':
     posApp().run()
