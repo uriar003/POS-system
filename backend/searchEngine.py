1599a4e4037da3ce0399e23022bb608f27b26dcc
SeanP <seanep94@gmail.com>
2022-12-02 14:47:18 -0800

    Added node functioanlity to search

--- backend/searchEngine.py
+++ backend/searchEngine.py
@@ -29,7 +29,7 @@ class SearchEngine:
         #self.inventory_df = ...
         # Get a SQL query (Maybe just do a test Excel for now... )
 
-    def user_input(self):
+    def user_input(self,nodes=False):
         '''
         THIS IS A TESTING FUNCTION, WILL NOT MAKE IT TO LIVE.
         Used to test the search_product function.
@@ -37,7 +37,10 @@ class SearchEngine:
         #here
         inp = input('Search for products.\nInput: ')
         countOf = 1
-        genObj = self.search_product(inp)
+        if nodes:
+            genObj = self.search_product_nodes(inp)
+        else:
+            genObj = self.search_product(inp)
         countMax = next(genObj) 
         for nodes in genObj:
             #count = nodes[0]
@@ -72,6 +75,24 @@ class SearchEngine:
                 nodeArray = []
         yield nodeArray
 
+    def search_product_nodes(self, userInput:str, amount:int=4):
+        '''
+        This is a generator function that will be called and will give the user back {amount} many ProductNodes in a list, along with the length of the dataframe.
+        '''
+        df = self.search(userInput) # Get the Dataframe of the objects.
+        nodeArray = []
+        counter = 0
+        yield df.shape[0]
+        for node in self.gen_data_nodes(df):
+            nodeArray.append(node)
+            counter += 1
+            if counter == amount:
+                counter = 0
+                yield nodeArray
+                nodeArray = []
+        yield nodeArray
+
+
     def search(self, inp:str) -> pd.DataFrame:
         '''
         Given a string, will search the pandas dataframe for the value, followed 
@@ -100,38 +121,57 @@ class SearchEngine:
             #yield ProductNode(id, name, price)
             yield [id, name, price, barcode]
 
+    def gen_data_nodes(self,df, amount:int=12):
+        '''
+        A generator that will take in a dataframe, and will on yield return an {amount} back to the caller until empty.
+        '''
+        for _, row in df.iterrows():
+            id = row['ITEM_ID']
+            name = row['NAME']
+            barcode = row['BARECODE']
+            pic  = row['PICTURE']
+            stock = row['NUMBER']
+            price = row['PRICE']
+            description = row['DESCRIPTION']
+            yield ProductNode(id, name, barcode, pic, stock, price, description)
+
+
 class ProductNode:
     '''
     A ProductNode is an object that is meant to be used to communicate with
     the Gui the proper data and access it's information.
     '''
     
-    def __init__(self, id, name, price):
+    def __init__(self, id, name, barcode, pic, stock, price, description):
         print(id, name, price)
         self.__id = id
         self.__name = name
-        #self.__picture = f"../imgs/{picture}"
+        self.__barcode = barcode
+        self.__picture = pic
         self.__price = float('%.2f' % price)
-        #self.__stock = stock
-        #self.__description = description
+        self.__stock = stock
+        self.__description = description
 
     def getId(self):
         return self.__id
 
     def getName(self):
         return self.__name
+
+    def getBarcode(self):
+        return self.__barcode
     
-    #def getPicture(self):
-    #    return self.__picture
+    def getPicture(self):
+       return self.__picture
         
-    #def getStock(self):
-    #    return self.__stock
+    def getStock(self):
+        return self.__stock
 
     def getPrice(self):
         return self.__price
         
-    #def getDescription(self):
-    #    return self.__description
+    def getDescription(self):
+        return self.__description
 
     def __repr__(self):
         return 'ProductNode'
@@ -141,5 +181,5 @@ class ProductNode:
 
 
 # Unnote to test
-#x = SearchEngine()
-#x.user_input()
+x = SearchEngine()
+x.user_input(nodes=True)
